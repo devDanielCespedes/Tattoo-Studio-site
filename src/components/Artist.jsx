f3e16d56c575d5664c3b28717f2b90abe81debb4
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-12-31 22:06:56 -0300

    arrumando o css do cart que voltou

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class Artist extends React.Component {
     render() {
