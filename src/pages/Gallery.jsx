2fc8999e546bb875f362ce95ade7d5bb19c87278
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-01-02 13:40:19 -0300

    falta arrumar o botao da galeria para carregar o BG correto do btn selecionado

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import TattooGalery from '../components/TattooGalery';
 import SelectStyleSelecedGalery from '../components/SelectStyleGalery';
 class Galery extends React.Component {
-
+  // constructor() {
+  //   super();
+  //   this.handlerBGBtnSelectStyle = this.handlerBGBtnSelectStyle.bind(this);
+  // }
 
   componentDidMount() {
     const btnSelectStyleGalery = document.querySelectorAll('.selected-style-galery');
     btnSelectStyleGalery[0].style.backgroundColor = 'chocolate'
-}
+  }
+
     render() {
         return(
           <div className='galery-content'>
