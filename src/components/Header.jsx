d567b02c17bd80d648814e4f2c1f0dc6e73b763d
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-01-04 04:32:40 -0300

    adicionando funcionalidades do mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
 import Logo from '../images/logo.jpg';
 class Header extends React.Component {
+
+  constructor() {
+    super(); 
+
+    this.handleClickBarsMenu = this.handleClickBarsMenu.bind(this);
+  }
   
   componentDidMount() {
     setTimeout(() => {
@@ -12,10 +20,26 @@ class Header extends React.Component {
       logo.classList.add('logo-after');
     }, 1000)
   }
+
+  handleClickBarsMenu() {
+    const barsMenuBtn = document.querySelector('.close-menu-bars');
+    const logoAfter = document.querySelector('.logo-after'); 
+    const navLinksBar = document.querySelectorAll('.open-icon-bars a');
+    const currentPage = document.querySelector('.current-page-header');
+
+    currentPage.style.transform = 'translate(0%, 2375%)'
+    barsMenuBtn.classList.remove('close-menu-bars');
+    barsMenuBtn.classList.add('open-menu-bars');
+    logoAfter.classList.remove('logo-after');
+    logoAfter.classList.add('logo-after-menu-bars-open');
+    navLinksBar.forEach(a => a.style.display = 'block');
+
+  }
     render(){
       let { location, cart } = this.props;
         return(
             <div className='header-content'>
+              <button type='button' onClick={() => this.handleClickBarsMenu()} className='close-menu-bars'><FontAwesomeIcon icon={faBars} /></button>
               <div className='logo'>
                 <Link
                   to='/'
@@ -25,8 +49,8 @@ class Header extends React.Component {
                   />
                 </Link>
               </div>
-              <nav className='home-links'>
-              <p className='current-page-header'>{location.pathname === '/' ? 'home' : location.pathname.slice(1)}</p>
+              <nav className='home-links open-icon-bars'>
+                <p className='current-page-header'>{location.pathname === '/' ? 'home' : location.pathname.slice(1)}</p>
                 <Link
                     to='/'
                   >
